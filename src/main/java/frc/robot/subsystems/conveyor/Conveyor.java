7fd10871c23abc1b11b189a1e545c836443005e7
Barel Shlidor <barel@shlidor.org>
2021-02-19 14:07:39 +0200

    Single-thread

--- src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -7,12 +7,10 @@ import frc.robot.Constants;
 import frc.robot.Ports;
 import frc.robot.utils.DeadbandLaserSensor;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import static frc.robot.Constants.Conveyor.*;
 
 public class Conveyor extends SubsystemBase {
-    private static final AtomicInteger balls = new AtomicInteger(Constants.Conveyor.INITIAL_BALLS_AMOUNT);
+    private static int balls = Constants.Conveyor.INITIAL_BALLS_AMOUNT;
 
     private final TalonFX motor = new TalonFX(Ports.Conveyor.MOTOR);
     private final DeadbandLaserSensor shooterSensor = new DeadbandLaserSensor(Ports.Conveyor.SHOOTER_LASER_SENSOR,
@@ -39,14 +37,14 @@ public class Conveyor extends SubsystemBase {
      * @return the amount of balls in the conveyor.
      */
     private static int getBallsAmount() {
-        return balls.get();
+        return balls;
     }
 
-    public static synchronized boolean isConveyorFull() {
+    public static boolean isConveyorFull() {
         return getBallsAmount() >= MAX_BALLS_AMOUNT;
     }
 
-    public static synchronized boolean isConveyorEmpty() {
+    public static boolean isConveyorEmpty() {
         return getBallsAmount() <= 0;
     }
 
@@ -54,14 +52,14 @@ public class Conveyor extends SubsystemBase {
      * Increment by one the amount of balls (only programmatically).
      */
     private static void addBall() {
-        balls.incrementAndGet();
+        balls++;
     }
 
     /**
      * Decrement by one the amount of balls (only programmatically).
      */
     private static void removeBall() {
-        balls.decrementAndGet();
+        balls--;
     }
 
     /**
