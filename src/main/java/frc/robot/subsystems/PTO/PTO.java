339965fb9b1b40decbb49e3cca4413834ee84fb9
oreld <orel.david1502@gmail.com>
2021-02-21 19:09:21 +0200

    Add pto subsystem

---
+++ src/main/java/frc/robot/subsystems/PTO/PTO.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems.PTO;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Ports;
+
+/**
+ *
+ */
+public class PTO extends SubsystemBase {
+    private final TalonFX motor1 = new TalonFX(Ports.PTO.MOTOR_1);
+    private final TalonFX motor2 = new TalonFX(Ports.PTO.MOTOR_2);
+    private final DoubleSolenoid piston = new DoubleSolenoid(Ports.PTO.PISTON_FORWARD, Ports.PTO.PISTON_REVERSE);
+    private gearboxState state = gearboxState.shooter;
+
+    public void configureClimber() {
+        //Todo: take the configuration from the climber PR.
+    }
+
+    public void configureShooter() {
+        //Todo: take the configuration from the shooter PR.
+    }
+
+    public void changeState(gearboxState state) {
+        this.state = state;
+    }
+
+    public gearboxState getState() {
+        return state;
+    }
+
+    public TalonFX getMotor1() {
+        return motor1;
+    }
+
+    public TalonFX getMotor2() {
+        return motor2;
+    }
+
+
+    public void changePiston(boolean isClimber) {
+        if (isClimber) {
+            piston.set(DoubleSolenoid.Value.kForward);
+        } else {
+            piston.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        if (state == gearboxState.shooter) {
+            configureShooter();
+        } else {
+            configureClimber();
+        }
+    }
+
+
+    public enum gearboxState {
+        shooter, climber
+    }
+}
