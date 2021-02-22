d4d6a035a2b192cb616a78c539bbb3d5507d0f33
natymeitav <53867718+natymeitav@users.noreply.github.com>
2021-02-22 16:44:50 +0200

    Format Intake Ports.
    Co-authored-by: Barel Shlidor <42340005+barel13@users.noreply.github.com>

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,7 +1,8 @@
 package frc.robot;
 
 public class Ports {
-    public static final class Intake{
+    public static final class Intake {
+
         public static final int MOTOR = 0;
         public static final boolean IS_INVERTED = false;
         public static final int SOLENOID_RIGHT = 1;
