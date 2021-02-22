f5d4dc63472a0b7558d01b58451bbce203782017
natymeitav <53867718+natymeitav@users.noreply.github.com>
2021-02-22 16:45:21 +0200

    Rename IS_INVERTED to IS_MOTOR_INVERTED.
    Co-authored-by: Dan Katzuv <31829093+katzuv@users.noreply.github.com>

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,7 +4,7 @@ public class Ports {
     public static final class Intake {
 
         public static final int MOTOR = 0;
-        public static final boolean IS_INVERTED = false;
+        public static final boolean IS_MOTOR_INVERTED = false;
         public static final int SOLENOID_RIGHT = 1;
         public static final int SOLENOID_LEFT = 2;
     }
