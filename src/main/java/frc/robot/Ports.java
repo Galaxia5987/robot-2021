75add5c8a38a9e542171b7689211184e45921ca6
oreld <orel.david1502@gmail.com>
2021-03-22 11:10:53 +0200

    Invert motor

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,7 +3,7 @@ package frc.robot;
 public final class Ports {
     public static final class Conveyor {
         public static final int MOTOR = 41;
-        public static final boolean IS_MOTOR_INVERTED = false;
+        public static final boolean IS_MOTOR_INVERTED = true;
 
     }
 }
