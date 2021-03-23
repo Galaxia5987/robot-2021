46f107a6e8d4569615ae7ecef3012b0ad5b1b744
galaxia <galaxia@gmail.com>
2021-03-23 15:57:41 +0200

    Add the upper motor to the shooter

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.controller.LinearQuadraticRegulator;
 import edu.wpi.first.wpilibj.estimator.KalmanFilter;
@@ -15,6 +17,7 @@ import edu.wpi.first.wpiutil.math.VecBuilder;
 import edu.wpi.first.wpiutil.math.Vector;
 import edu.wpi.first.wpiutil.math.numbers.N1;
 import frc.robot.Constants;
+import frc.robot.Ports;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.PTO.PTO;
 import frc.robot.subsystems.UnitModel;
@@ -35,20 +38,22 @@ import static frc.robot.Constants.Shooter.*;
  * @since 2021
  */
 public class Shooter extends SubsystemBase {
-
+    private final TalonFX upMotor = new TalonFX(Ports.Shooter.UP);
     private final UnitModel unitModel = new UnitModel(TICKS_PER_ROTATION);
 
     private final LinearRegression velocityEstimator;
-    private final LinearSystemLoop<N1, N1, N1> stateSpacePredictor;
+    private LinearSystemLoop<N1, N1, N1> upMotorStateSpacePredictor;
+    private LinearSystemLoop<N1, N1, N1> stateSpacePredictor;
     private double lastOmega = 0;
     private final Timer shootingTimer = new Timer();
-    private double lastTime = 0;
-
-
+    private double lastTime = 1;
 
     public Shooter() {
-        this.stateSpacePredictor = constructLinearSystem();
+        this.stateSpacePredictor = constructLinearSystem(J.get());
         this.velocityEstimator = new LinearRegression(readCSV());
+
+        upMotor.setInverted(Ports.Shooter.UP_INVERTED);
+        this.upMotorStateSpacePredictor = constructLinearSystem(UP_MOTOR_J.get());
     }
 
     /**
@@ -56,7 +61,8 @@ public class Shooter extends SubsystemBase {
      *
      * @return an object that represents the model to reach the velocity at the best rate.
      */
-    private LinearSystemLoop<N1, N1, N1> constructLinearSystem() {
+    private LinearSystemLoop<N1, N1, N1> constructLinearSystem(double J) {
+        if (J == 0) throw new RuntimeException("J must have non-zero value");
         // https://file.tavsys.net/control/controls-engineering-in-frc.pdf Page 76
         Vector<N1> A = VecBuilder.fill(-Math.pow(GEAR_RATIO, 2) * kT / (kV * OMEGA * J));
         Vector<N1> B = VecBuilder.fill((GEAR_RATIO * kT) / (OMEGA * J));
@@ -98,6 +104,10 @@ public class Shooter extends SubsystemBase {
         return unitModel.toVelocity(RobotContainer.pto.getMaster().getSelectedSensorVelocity());
     }
 
+    public double getCurrent() {
+        return RobotContainer.pto.getMaster().getSupplyCurrent();
+    }
+
     /**
      * Set the velocity to apply by the motor.
      *
@@ -121,8 +131,8 @@ public class Shooter extends SubsystemBase {
         stateSpacePredictor.predict(timeInterval);
 
         double voltageToApply = stateSpacePredictor.getU(0); // u = input, calculated by the input.
-        // returns the voltage to apply (between 0 and 12)
-        setPower(voltageToApply / Constants.NOMINAL_VOLTAGE); // map to be between 0 and 1
+        // returns the voltage to apply (between -12 and 12)
+        setPower(voltageToApply / Constants.NOMINAL_VOLTAGE); // map to be between -1 and 1
     }
 
     /**
@@ -132,12 +142,51 @@ public class Shooter extends SubsystemBase {
      * @see #stop()
      */
     public void setPower(double power) {
-        if (RobotContainer.pto.getState() == PTO.GearboxState.SHOOTER) {
-            return;
-        }
+        if (RobotContainer.pto.getState() == PTO.GearboxState.CLIMBER) return;
 
         RobotContainer.pto.getMaster().set(TalonFXControlMode.PercentOutput, power,
                 DemandType.ArbitraryFeedForward, Constants.Shooter.ARBITRARY_FEED_FORWARD);
+        RobotContainer.pto.getSlave().set(TalonFXControlMode.PercentOutput, power,
+                DemandType.ArbitraryFeedForward, Constants.Shooter.ARBITRARY_FEED_FORWARD);
+    }
+
+    /**
+     * Set the velocity to apply by the motor.
+     *
+     * @param velocity the desired velocity at which the motor will rotate. [RPS]
+     * @see #setVelocity(double, double)
+     */
+    public void setVelocityUp(double velocity) {
+        setVelocityUp(velocity, Constants.LOOP_PERIOD);
+    }
+
+    /**
+     * Set the velocity to apply by the motor.
+     *
+     * @param velocity     the desired velocity at which the motor will rotate. [RPS]
+     * @param timeInterval the time interval from the last call of this function. [sec]
+     * @see #setPower(double)
+     */
+    public void setVelocityUp(double velocity, double timeInterval) {
+        upMotorStateSpacePredictor.setNextR(VecBuilder.fill(velocity)); //r = reference (setpoint)
+        upMotorStateSpacePredictor.correct(VecBuilder.fill(getVelocity()));
+        upMotorStateSpacePredictor.predict(timeInterval);
+
+        double voltageToApply = upMotorStateSpacePredictor.getU(0); // u = input, calculated by the input.
+        // returns the voltage to apply (between -12 and 12)
+        setPowerUp(voltageToApply / Constants.NOMINAL_VOLTAGE); // map to be between -1 and 1
+    }
+
+    /**
+     * Set the power to apply by the motor.
+     *
+     * @param power the power at which the motor will rotate. [percentage, between -1 and 1]
+     * @see #stop()
+     */
+    public void setPowerUp(double power) {
+        if (RobotContainer.pto.getState() == PTO.GearboxState.CLIMBER) return;
+
+        upMotor.set(ControlMode.PercentOutput, power, DemandType.ArbitraryFeedForward, ARBITRARY_FEED_FORWARD_UP);
     }
 
     /**
@@ -165,17 +214,25 @@ public class Shooter extends SubsystemBase {
      */
     public void stop() {
         setPower(0);
+        setPowerUp(0);
     }
 
     @Override
     public void periodic() {
+        shootingTimer.start();
         final double currentTime = shootingTimer.get();
         double omega = getVelocity() * Units.inchesToMeters(4);
         FireLog.log("velocity", getVelocity());
+        FireLog.log("up-velocity", unitModel.toVelocity(upMotor.getSelectedSensorVelocity()));
         FireLog.log("radial-velocity", omega);
+        FireLog.log("setpoint", RobotContainer.velocity.get());
         FireLog.log("accl-omega", (omega - lastOmega) / (currentTime - lastTime));
         FireLog.log("voltage", 12);
+        FireLog.log("current", getCurrent());
+        lastTime = currentTime;
 
         lastOmega = omega;
+        this.stateSpacePredictor = constructLinearSystem(J.get());
+        this.upMotorStateSpacePredictor = constructLinearSystem(UP_MOTOR_J.get());
     }
 }
