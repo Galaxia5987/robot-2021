a7a8c89fe54f1b998568f9e70732754e31a2c8d9
galaxia <galaxia@gmail.com>
2021-03-23 16:10:32 +0200

    Create another constructor to AdjustHood command

--- src/main/java/frc/robot/subsystems/shooter/commands/AdjustHood.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/AdjustHood.java
@@ -1,23 +1,24 @@
 package frc.robot.subsystems.shooter.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.shooter.Shooter;
 
 public class AdjustHood extends CommandBase {
 
     private final Shooter shooter;
-    private final double distance;
+    private final Shooter.State state;
 
     public AdjustHood(Shooter shooter, double distance) {
-        this.shooter = shooter;
-        this.distance = distance;
+        this(shooter, Shooter.State.getOptimalState(distance));
     }
 
+    public AdjustHood(Shooter shooter, Shooter.State state) {
+        this.shooter = shooter;
+        this.state = state;
+    }
 
     @Override
     public void execute() {
-//        shooter.changeState(Shooter.State.getOptimalState(distance));
-        shooter.hoodMotor.set(-0.2);
+        shooter.changeState(state);
     }
 }
