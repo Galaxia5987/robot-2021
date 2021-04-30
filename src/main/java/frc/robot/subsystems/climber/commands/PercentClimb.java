1553894c7e923b7ca947bdd095743b94c006d4d9
galaxia <galaxia@gmail.com>
2021-04-30 13:57:29 +0300

    Create PercentClimb command

---
+++ src/main/java/frc/robot/subsystems/climber/commands/PercentClimb.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.climber.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.climber.Climber;
+
+import java.util.function.DoubleSupplier;
+
+public class PercentClimb extends CommandBase {
+
+    private final Climber climber;
+    private final DoubleSupplier power;
+
+    public PercentClimb(Climber climber, DoubleSupplier power) {
+        this.climber = climber;
+        this.power = power;
+        addRequirements(climber);
+    }
+
+    @Override
+    public void execute() {
+        System.out.println(power.getAsDouble());
+        climber.setPower(power.getAsDouble());
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.setStopperMode(Climber.PistonMode.CLOSED);
+    }
+}
