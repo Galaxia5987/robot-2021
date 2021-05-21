ce29e34c4f929137caefde659bc7fb888159c638
galaxia <galaxia@gmail.com>
2021-05-21 10:05:53 +0300

    Remove position boolean in Intake subsystem

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,35 +2,31 @@ package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Ports;
 
 /**
  * this subsystem takes the balls from the field to the funnel.
  */
 public class Intake extends SubsystemBase {
-    private TalonFX motor = new TalonFX(Ports.Intake.MOTOR);
-    private Solenoid solenoid = new Solenoid(Ports.Intake.SOLENOID);
-    private boolean position;
+    private final TalonFX motor = new TalonFX(Ports.Intake.MOTOR);
+    private final Solenoid solenoid = new Solenoid(Ports.Intake.SOLENOID);
 
     public Intake() {
         motor.setInverted(Ports.Intake.IS_MOTOR_INVERTED);
-        position = false;
     }
 
     /**
      * @return the state of the solenoids
      */
     public boolean isOpen() {
-        return position;
+        return solenoid.get();
     }
 
     /**
      * this function sets the motor's velocity
+     *
      * @param speed - the target velocity (m/s)
      */
     public void setVelocity(double speed) {
@@ -41,7 +37,6 @@ public class Intake extends SubsystemBase {
      * toggles piston's state (opened --> closed || closed -- > opened)
      */
     public void togglePiston() {
-        solenoid.set(!position);
-        position = !position;
+        solenoid.toggle();
     }
 }
