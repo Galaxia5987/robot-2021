7befba9815f918cc2042612c46b8a113ff14d664
Galaxia <Galaxia@gmail.com>
2021-06-03 22:10:46 +0300

    Fix Hood

--- src/main/java/frc/robot/subsystems/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/hood/Hood.java
@@ -9,11 +9,15 @@ import frc.robot.Ports;
 import frc.robot.UnitModel;
 import frc.robot.subsystems.shooter.LinearRegression;
 import org.apache.commons.lang.math.DoubleRange;
+import org.techfire225.webapp.FireLog;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.IntSupplier;
 
-import static frc.robot.Constants.Shooter.*;
+import static frc.robot.Constants.Shooter.TICKS_PER_ROTATION;
 
 public class Hood extends SubsystemBase {
     private final UnitModel unitModel = new UnitModel(TICKS_PER_ROTATION);
@@ -53,16 +57,28 @@ public class Hood extends SubsystemBase {
      *
      * @param newState the new state of the hood.
      */
-    public void changeState(State newState) {
-        if (newState.position < Constants.Hood.MAX_POSITION && newState.position > Constants.Hood.MIN_POSITION) {
+    public void changePosition(State newState) {
+        if (newState.position.getAsInt() < Constants.Hood.MAX_POSITION && newState.position.getAsInt() > Constants.Hood.MIN_POSITION) {
             double gain = 0.02;
             if (Constants.Hood.STUCK_POSITION - getPosition() > 0) {
                 gain *= -1;
             }
-//yy            hoodMotor.set(ControlMode.MotionMagic, newState.position, DemandType.ArbitraryFeedForward,
+//yy            hoodMotor.set(ControlMode.MotionMagic, newState.position.getAsInt(), DemandType.ArbitraryFeedForward,
 //                    Constants.Hood.HOOD_ARBITRARY_KF.get() / ((STUCK_POSITION - getHoodPosition())/ MAX_HOOD_POSITION + gain));
-            motor.set(ControlMode.MotionMagic, newState.position);
+            motor.set(ControlMode.MotionMagic, newState.position.getAsInt());
             this.state = newState;
+            System.out.println(Constants.Hood.ARBITRARY_KF.get() / ((Constants.Hood.STUCK_POSITION - getPosition()) / Constants.Hood.MAX_POSITION + gain));
+        }
+    }
+
+    public void changePosition(int position) {
+        if (position < Constants.Hood.MAX_POSITION && position > Constants.Hood.MIN_POSITION) {
+            double gain = 0.02;
+            if (Constants.Hood.STUCK_POSITION - getPosition() > 0) {
+                gain *= -1;
+            }
+            motor.set(ControlMode.MotionMagic, position);
+//            this.state = newState;
             System.out.println(Constants.Hood.ARBITRARY_KF.get() / ((Constants.Hood.STUCK_POSITION - getPosition()) / Constants.Hood.MAX_POSITION + gain));
         }
     }
@@ -91,18 +107,35 @@ public class Hood extends SubsystemBase {
         motor.set(ControlMode.PercentOutput, 0);
     }
 
+    public void resetPosition() {
+//        Constants.Hood.MIN_POSITION = motor.getSelectedSensorPosition();
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        FireLog.log("position", getPosition());
+    }
+
     public enum State {
         CLOSED(Constants.Hood.MIN_POSITION + 200, new DoubleRange(0, 0), "/Low.csv"),
-        LOW(Constants.Hood.MIN_POSITION + 1500, new DoubleRange(0, 0), "/Low.csv"),
-        MIDDLE(Constants.Hood.MIN_POSITION + 3500, new DoubleRange(0, 0), "/Middle.csv"),
-        HIGH(Constants.Hood.MIN_POSITION + 4000, new DoubleRange(0, 0), "/High.csv"),
+        LOW(Constants.Hood.MIN_POSITION + 1500, new DoubleRange(0.75, 1.7), "/Low.csv"),
+        MIDDLE_LOW(Constants.Hood.MIN_POSITION + 2500, new DoubleRange(1.7, 2.5), "/Middle_Low.csv"),
+        MIDDLE(Constants.Hood.MIN_POSITION + 3000, new DoubleRange(2.5, 3.3), "/Middle.csv"),
+        HIGH(Constants.Hood.MIN_POSITION + 3450, new DoubleRange(3.3, 4.5), "/High.csv"),
+        ABOVE_HIGH(() -> Constants.Hood.MIN_POSITION + 3750, new DoubleRange(4.5, 6), "/Above_High.csv"),
         OPEN(Constants.Hood.MAX_POSITION - 200, new DoubleRange(0, 0), "/High.csv");
 
         public final DoubleRange shootingRange; // [min, max] meters
         public final LinearRegression velocityEstimator;
-        public final int position; //[ticks]
+        public final IntSupplier position; //[ticks]
 
         State(int position, DoubleRange range, String pathToCsv) {
+            this(() -> position, range, pathToCsv);
+        }
+
+        State(IntSupplier position, DoubleRange range, String pathToCsv) {
             this.position = position;
             this.shootingRange = range;
             this.velocityEstimator = new LinearRegression(readCSV(pathToCsv));
@@ -115,7 +148,11 @@ public class Hood extends SubsystemBase {
          * @return the optimal state.
          */
         public static State getOptimalState(double distance) {
-            State current = CLOSED;
+            var filtered = Arrays.stream(State.values())
+                    .filter(state -> state.shootingRange.getMinimumDouble() < distance && state.shootingRange.getMaximumDouble() > distance)
+                    .min(Comparator.comparingDouble(state -> state.velocityEstimator.estimateVelocityFromDistance(distance)));
+            return filtered.orElse(State.CLOSED);
+        /*    State current = CLOSED;
             double minVelocity = CLOSED.velocityEstimator.estimateVelocityFromDistance(distance);
             for (State state : State.values()) {
                 double currentVelocity = state.velocityEstimator.estimateVelocityFromDistance(distance);
@@ -125,7 +162,10 @@ public class Hood extends SubsystemBase {
                 }
             }
             return current;
+        */
         }
+
+
 
         /**
          * Internal function to create an input stream reader, in order values from the shooting experiments.
