d3f8f8f5333064e4344d2238bf5dfba2a51c636b
Galaxia <Galaxia@gmail.com>
2021-06-03 22:06:57 +0300

    Fix AdjustHood

--- src/main/java/frc/robot/subsystems/hood/commands/AdjustHood.java
+++ src/main/java/frc/robot/subsystems/hood/commands/AdjustHood.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.hood.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.hood.Hood;
 import org.techfire225.webapp.FireLog;
 
+import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 public class AdjustHood extends CommandBase {
@@ -12,12 +14,12 @@ public class AdjustHood extends CommandBase {
     private final Hood hood;
     private final Supplier<Hood.State> state;
 
-    public AdjustHood(Hood hood, double distance) {
-        this(hood, Hood.State.getOptimalState(distance));
+    public AdjustHood(Hood hood, DoubleSupplier distance) {
+        this(hood, () -> Hood.State.getOptimalState(distance.getAsDouble()));
     }
 
     public AdjustHood(Hood hood, Hood.State state) {
-       this(hood, ()-> state);
+        this(hood, () -> state);
     }
 
     public AdjustHood(Hood hood, Supplier<Hood.State> state) {
@@ -30,13 +32,15 @@ public class AdjustHood extends CommandBase {
     @Override
     public void execute() {
         hood.updatePID();
-        hood.changeState(state.get());
-        FireLog.log("setpoint", state.get().position);
+        hood.changePosition(state.get());
+        System.out.println(state.get());
+        FireLog.log("setpoint", state.get().position.getAsInt());
+        SmartDashboard.putString("hood-state", state.get().toString());
     }
 
     @Override
     public boolean isFinished() {
-        return Math.abs(hood.getPosition() - state.get().position) < Constants.Hood.POSITION_TOLERANCE;
+        return Math.abs(hood.getPosition() - state.get().position.getAsInt()) < Constants.Hood.POSITION_TOLERANCE;
     }
 
     @Override
