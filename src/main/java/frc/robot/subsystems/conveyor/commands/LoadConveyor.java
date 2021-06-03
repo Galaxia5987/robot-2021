a0b11fb9ffa913d6acdd4a456337dc43dc0dabcf
Galaxia <Galaxia@gmail.com>
2021-06-03 22:11:16 +0300

    Change LoadConveyor command

--- src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.conveyor.commands;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.funnel.Funnel;
 
@@ -13,16 +15,33 @@ public class LoadConveyor extends CommandBase {
     private final Conveyor conveyor;
     private final double power;
     private final Timer timer = new Timer();
+    private final Funnel funnel;
 
-    public LoadConveyor(Conveyor conveyor, double power) {
+    public  LoadConveyor(Conveyor conveyor, double power, Funnel funnel) {
         this.conveyor = conveyor;
         this.power = power;
+        this.funnel = funnel;
 
         addRequirements(conveyor);
     }
 
     @Override
     public void execute() {
+       /* if (Conveyor.hasFunnelSensedObject() && Conveyor.hasNavxFunnelSensedObject()) {
+            funnel.setPower(0);
+            conveyor.setPower(Constants.Conveyor.CONVEYOR_MOTOR_POWER);
+        } else if (Conveyor.hasNavxFunnelSensedObject()) {
+            funnel.setPower(Constants.Funnel.POWER);
+            conveyor.setPower(0);
+        } else if (Conveyor.hasFunnelSensedObject()) {
+            funnel.setPower(0);
+            conveyor.setPower(Constants.Conveyor.CONVEYOR_MOTOR_POWER);
+        } else {
+            funnel.setPower(Constants.Funnel.POWER);
+            conveyor.setPower(0);
+        }
+*/
+
         if (Conveyor.hasFunnelSensedObject() && !timer.hasElapsed(2)) {
             timer.start();
             conveyor.setPower(moderatePower(timer.get(), 2));
@@ -52,6 +71,7 @@ public class LoadConveyor extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         conveyor.stop();
-        timer.stop();
+//        timer.stop();
+        funnel.setPower(0);
     }
 }
