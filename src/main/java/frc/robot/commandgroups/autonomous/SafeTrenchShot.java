933409ccaf088ddd33cc01aae639379fbf66b639
Galaxia <Galaxia@gmail.com>
2021-06-03 22:14:32 +0300

    Start Auto

--- src/main/java/frc/robot/commandgroups/autonomous/SafeTrenchShot.java
+++ src/main/java/frc/robot/commandgroups/autonomous/SafeTrenchShot.java
@@ -15,12 +15,13 @@ import frc.robot.subsystems.funnel.Funnel;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.ToggleIntake;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.utils.VisionModule;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class SafeTrenchShot extends SequentialCommandGroup {
-    public SafeTrenchShot(SwerveDrive swerve, Intake intake, Funnel funnel, Conveyor conveyor, Shooter shooter, PTO pto) {
+    public SafeTrenchShot(SwerveDrive swerve, Intake intake, Funnel funnel, Conveyor conveyor, Shooter shooter, VisionModule vision, PTO pto) {
         Trajectory initiationToSafeTrench = new Trajectory(), safeTrenchToShoot = new Trajectory();
         try {
             Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(Constants.Autonomous.INITIATION_TO_SAFE_TRENCH_PATH);
@@ -31,7 +32,7 @@ public class SafeTrenchShot extends SequentialCommandGroup {
             System.out.println("Couldn't find the autonomous file...");
             ex.printStackTrace();
         }
-        addCommands(new FollowPath(swerve, initiationToSafeTrench), new PickupBalls(intake, funnel, conveyor, Constants.Intake.POWER::get, true).withTimeout(2), new ToggleIntake(intake),
-                new FollowPath(swerve, safeTrenchToShoot), new FeedAndShoot(pto, conveyor, shooter, Constants.Conveyor.CONVEYOR_MOTOR_POWER).withTimeout(4));
+        addCommands(new FollowPath(swerve, initiationToSafeTrench), new PickupBalls(intake, funnel, conveyor, Constants.Intake.POWER::get, true).withTimeout(2), new ToggleIntake(intake));
+//                new FollowPath(swerve, safeTrenchToShoot), new FeedAndShoot(pto, conveyor, shooter, hood, vision, Constants.Conveyor.CONVEYOR_MOTOR_POWER).withTimeout(4));
     }
 }
