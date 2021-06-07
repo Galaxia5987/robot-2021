ab1e9ab2ae0ef1a8c6d846e68094ffe799967d40
Galaxia <Galaxia@gmail.com>
2021-06-07 22:33:48 +0300

    Create Outtake command for shooting

--- src/main/java/frc/robot/commandgroups/Outtake.java
+++ src/main/java/frc/robot/commandgroups/Outtake.java
@@ -8,12 +8,15 @@ import frc.robot.subsystems.funnel.Funnel;
 import frc.robot.subsystems.funnel.commands.StartFunnel;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.StartIntake;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.commands.ShootWithoutVision;
 
 public class Outtake extends ParallelCommandGroup {
-    public Outtake(Funnel funnel, Conveyor conveyor) {
+    public Outtake(Funnel funnel, Conveyor conveyor, Shooter shooter) {
         addCommands(
                 new StartFunnel(funnel, false),
-                new FeedShooter(conveyor, -Constants.Conveyor.CONVEYOR_MOTOR_POWER)
+                new FeedShooter(conveyor, -Constants.Conveyor.CONVEYOR_MOTOR_POWER_SLOW),
+                new ShootWithoutVision(shooter, -20)
         );
     }
 
